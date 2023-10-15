c08726a20e8a66cb19f5d5ef80e0afd7f654cd91
Jayden  Elishaw <80910384+BluJay131@users.noreply.github.com>
2023-10-15 12:30:10 -0700

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,2 +1,2 @@
-cd /home/jadmin/Desktop/Firewatch
+cd /home/jadmin/Desktop/Firewatch (YOUR YOLOV5 FOLDER DESTINATION HERE)
 python detectFire.py --source 0 --weights best.pt --conf 0.15 --img-size 96
